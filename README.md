860a3cbebca6b5d098eb80f3488c22fded32272d
Godzilla <jason@theograys.com>
2013-05-04 08:39:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Face Detector
+=============
+
+This is part of the example up through chapter four of Joseph Howse's [OpenCV Computer Vision with Python](http://www.amazon.com/OpenCV-Computer-Vision-Python-Joseph/dp/1782163921/). I recommend you get a copy for a detailed explanation of every class and method, as well as a good read.
+
+I put this up because the links provided in the book to download the source code did not work.
---
+++ cameo.py
@@ -0,0 +1,64 @@
+import cv2
+import filters
+from managers import WindowManager, CaptureManager
+import rects
+from trackers import FaceTracker
+
+class Cameo(object):
+    
+    def __init__(self):
+        self._windowManager = WindowManager('Cameo',
+                                            self.onKeypress)
+        self._captureManager = CaptureManager(
+            cv2.VideoCapture(0), self._windowManager, True)
+        self._faceTracker = FaceTracker()
+        self._shouldDrawDebugRects = False
+        self._curveFilter = filters.BGRPortraCurveFilter()
+    
+    def run(self):
+        """Run the main loop."""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            self._captureManager.enterFrame()
+            frame = self._captureManager.frame
+            
+            self._faceTracker.update(frame)
+            faces = self._faceTracker.faces
+            rects.swapRects(frame, frame,
+                            [face.faceRect for face in faces])
+            
+            #Filter example
+            #filters.strokeEdges(frame, frame)
+            #self._curveFilter.apply(frame, frame)
+            
+            if self._shouldDrawDebugRects:
+                self._faceTracker.drawDebugRects(frame)
+            
+            self._captureManager.exitFrame()
+            self._windowManager.processEvents()
+    
+    def onKeypress(self, keycode):
+        """Handle a keypress.
+        
+        space  -> Take a screenshot.
+        tab    -> Start/stop recording a screencast.
+        x      -> Start/stop drawing debug rectangles around faces.
+        escape -> Quit.
+        
+        """
+        if keycode == 32: # space
+            self._captureManager.writeImage('screenshot.png')
+        elif keycode == 9: # tab
+            if not self._captureManager.isWritingVideo:
+                self._captureManager.startWritingVideo(
+                    'screencast.avi')
+            else:
+                self._captureManager.stopWritingVideo()
+        elif keycode == 120: # x
+            self._shouldDrawDebugRects = \
+                not self._shouldDrawDebugRects
+        elif keycode == 27: # escape
+            self._windowManager.destroyWindow()
+
+if __name__=="__main__":
+   Cameo().run()
